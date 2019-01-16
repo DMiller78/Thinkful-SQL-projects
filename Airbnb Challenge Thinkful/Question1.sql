42fd51318a6a13a9a85f81a8ad055d63203185bf
Dane Miller <35052069+DMiller78@users.noreply.github.com>
2019-01-15 22:46:16 -0800

    Update Question1.sql

--- Airbnb Challenge Thinkful/Question1.sql
+++ Airbnb Challenge Thinkful/Question1.sql
@@ -1,3 +1,6 @@
+
+#Question 1: What's the most expensive listing? What else can you tell me about the listing?
+
 SELECT * FROM sfo_reviews LIMIT 1; 
 
 SELECT 
@@ -10,3 +13,5 @@ FROM
 	sfo_listings
 ORDER BY price DESC 
 LIMIT 3;
+
+# The most expensive listing is located in Western Additon, Full Victoria house 7500 square feet. The house is listed for $10,000.
