c22c8864c69d2ce8ad5b0db06514b32ac25d4d38
Dane Miller <35052069+DMiller78@users.noreply.github.com>
2019-01-15 22:48:25 -0800

    Update Question2.sql

--- Airbnb Challenge Thinkful/Question2.sql
+++ Airbnb Challenge Thinkful/Question2.sql
@@ -1,3 +1,6 @@
+#Question 2: What neighborhoods seem to be the most popular?
+
+
 SELECT 
 	neighbourhood,
 	COUNT(*)
@@ -9,3 +12,5 @@ ON
     sfo_listings.id = sfo_calendar.listing_id
 GROUP BY sfo_listings.neighbourhood
 ORDER BY COUNT(*) DESC;
+
+# The top 3 neighbourhoods in SF are Mission, Western Addition, and Downtown Civic Center. 
